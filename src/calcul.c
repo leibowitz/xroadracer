7b6f4332f290f9ef8e4472cfeda5bf68a8783ab6
Gianni Moschini <gianni.moschini+github@gmail.com>
2017-12-29 21:53:01 +0000

    Add rotated_xy

--- src/calcul.c
+++ src/calcul.c
@@ -1,3 +1,10 @@
+#include <math.h>
+
+void rotated_xy(float x, float y, float rotation, float *tx, float *ty) {
+        float rad = rotation * M_PI/180.0;
+	*tx = x * cos(rad) - y * sin(rad);
+	*ty = x * sin(rad) + y * cos(rad);
+}
 
 int min(double a, double b)
 {
--- src/calcul.h
+++ src/calcul.h
@@ -1,8 +1,9 @@
 #ifndef __CALCUL_H__
 #define __CALCUL_H__
 
-int min(double a, double b);
+void rotated_xy(float x, float y, float rotation, float *tx, float *ty);
 
+int min(double a, double b);
 
 int max(double a, double b);
 

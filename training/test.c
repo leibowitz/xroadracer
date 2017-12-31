2c82641834637eea6db6641401d198b2fc1138e1
Gianni Moschini <gianni.moschini+github@gmail.com>
2017-12-31 20:05:03 +0000

    Update training script

--- training/test.c
+++ training/test.c
@@ -4,12 +4,15 @@
 int main(int argc, char* argv[])
 {
         FILE *fp;
-        if(argc < 1) {
+        if(argc < 2) {
                 exit(1);
         }
         printf("%s\n", argv[1]);
+        char name[20];
+        sprintf(name, "data%s.txt", argv[1]);
+        printf("%s\n", name);
 
-        if( (fp = fopen("data.txt", "r+")) == NULL)
+        if( (fp = fopen(name, "r+")) == NULL)
         {
                 printf("o\n");
                 exit(1);
@@ -20,34 +23,40 @@ if(fp == NULL) {
 }
 int s;
 float *output;
+        sprintf(name, "data%s.net", argv[1]);
  struct fann *ann = fann_create_from_file(
- "data.net");
+ name);
 //1276 8 4
 //
 //0.000000 80 101 182 199 103 44 30 
 //
 int i, j, k;
  float input[8];
-while(fscanf(fp,"%d %d %d", &i, &j, &k) == 3)  
+fscanf(fp,"%d %d %d\n", &i, &j, &k);
+        // skip line
+        fscanf(fp, "%*[^\n]\n", NULL);
+while(fscanf(fp,"%f %f %f %f %f %f %f %f \n", &input[0], &input[1], &input[2], &input[3], &input[4], &input[5], &input[6], &input[7]) == 8)  
 {  
-       //printf("%d %d %d\n", i, j, k);  
-        // skip line
-        fscanf(fp, "%*[^\n]\n", NULL);
-        // read input
-        fscanf(fp,"%f %f %f %f %f %f %f %f", &input[0], &input[1], &input[2], &input[3], &input[4], &input[5], &input[6], &input[7]);
-        // skip line
-        fscanf(fp, "%*[^\n]\n", NULL);
        printf("%f %f %f %f %f %f %f %f\n", input[0], input[1], input[2], input[3], input[4], input[5], input[6], input[7]);  
  output = fann_run(ann, input);
- s = sizeof(*output);
+ //s = sizeof(*output);
+ s = 1;
  for(i = 0; i < s; i++) {
   printf("%d: %d\n", i, (int)roundf(output[i]));
  }
+       //printf("%d %d %d\n", i, j, k);  
+        // skip line
+        fscanf(fp, "%*[^\n]\n", NULL);
+        // skip line
+        //fscanf(fp, "%*[^\n]\n", NULL);
+        // skip line
+        fscanf(fp, "%*[^\n]\n", NULL);
 }  
 if(feof(fp))  
 {            
          puts("EOF");
 }  
+printf("done\n");
  //if(argc != 2) error("Remember to specify an input file");
  return 0;
 }
--- training/train.c
+++ training/train.c
@@ -1,26 +1,83 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 #include "fann.h"
+
+int random_number(int min_num, int max_num)
+{
+    int result = 0, low_num = 0, hi_num = 0;
+
+    if (min_num < max_num)
+    {
+        low_num = min_num;
+        hi_num = max_num + 1; // include max_num in output
+    } else {
+        low_num = max_num + 1; // include max_num in output
+        hi_num = min_num;
+    }
+
+    result = (rand() % (hi_num - low_num)) + low_num;
+    return result;
+}
 
 int main()
 {
+        srand(time(NULL));
+
+        int layers = 3;
+        int ninputs = 19;
+        //int hidden_n = 7;
+        int hidden_n = random_number(3, 40);
+        printf("%d\n", hidden_n);
+        int noutput = 1;
+
+        int iter = 200000;
+        int p = 50;
+
+        float r1 = 0.001;
+        float r2 = 0.001;
+        //float r3 = 0.15;
+        float r3 = 0.08;
+        float r4 = 0.08;
  struct fann *ann;
- ann = fann_create_standard(3, 8, 15, 1);
- fann_train_on_file(ann, "data1.txt", 2000, 10, 0.01);
- fann_save(ann, "data1.net");
+ 
+ ann = fann_create_standard(layers, ninputs, hidden_n, noutput);
+ printf("fw\n");
+ fann_train_on_file(ann, "map1_fw.txt", iter, p, r1);
+ fann_save(ann, "map1_fw.net");
+ //fann_print_parameters(ann);
  fann_destroy(ann);
+ 
 
- ann = fann_create_standard(3, 8, 15, 1);
- fann_train_on_file(ann, "data2.txt", 2000, 10, 0.01);
- fann_save(ann, "data2.net");
+ 
+ ann = fann_create_standard(layers, ninputs, hidden_n, noutput);
+ printf("bw\n");
+ fann_train_on_file(ann, "map1_bw.txt", iter, p, r2);
+ fann_save(ann, "map1_bw.net");
+ //fann_print_parameters(ann);
  fann_destroy(ann);
+ 
 
- ann = fann_create_standard(3, 8, 15, 1);
- fann_train_on_file(ann, "data3.txt", 2000, 10, 0.01);
- fann_save(ann, "data3.net");
+ 
+ ann = fann_create_standard(layers, ninputs, hidden_n, noutput);
+ printf("right\n");
+ fann_train_on_file(ann, "map1_right.txt", iter, p, r3);
+ fann_save(ann, "map1_right.net");
+ //fann_print_parameters(ann);
  fann_destroy(ann);
+ 
 
- ann = fann_create_standard(3, 8, 15, 1);
- fann_train_on_file(ann, "data4.txt", 2000, 10, 0.01);
- fann_save(ann, "data4.net");
+ 
+ //ann = fann_create_standard(2, ninputs, noutput);
+ ann = fann_create_standard(layers, ninputs, hidden_n, noutput);
+ printf("left\n");
+ //ann = fann_create_standard(4, ninputs, 10, 6, noutput);
+ //ann = fann_create_standard(5, ninputs, 10, 10, 12, noutput);
+ //ann = fann_create_standard(6, ninputs, 10, 10, 8, 6, noutput);
+ //fann_train_on_file(ann, "map1_left.txt", iter, p, 0.168);
+ fann_train_on_file(ann, "map1_left.txt", iter, p, r4);
+ fann_save(ann, "map1_left.net");
+ //fann_print_parameters(ann);
  fann_destroy(ann);
  return 0;
 }
